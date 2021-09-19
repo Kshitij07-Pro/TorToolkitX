3a98330322b059835d24d4fbd03f9ac2eb1e630f
Kshitij07-Pro <85276195+Kshitij07-Pro@users.noreply.github.com>
2021-09-19 14:32:57 +0530

    Update ttk_ytdl.py

--- tortoolkit/core/ttk_ytdl.py
+++ tortoolkit/core/ttk_ytdl.py
@@ -626,16 +626,16 @@ async def print_files(e, files):
 
     for i in files.keys():
         link = f"https://t.me/c/{str(chat_id)[4:]}/{files[i]}"
-        msg += f'🚩 <a href="{link}">{i}</a>\n'
+        msg += f'➩ <a href="{link}">{i}</a>\n'
 
     rmsg = await e.client.get_messages(e.chat_id, ids=e.message_id)
     rmsg = await rmsg.get_reply_message()
     if rmsg is None:
         # msg += "\n<a href='tg://user?id={}'>Done<a>".format(rmsg.sender_id)
-        msg += "\n<a href='tg://user?id={}'>Done</a>".format(e.sender_id)
+        msg += "\n📌 <a href='tg://user?id={}'>Done</a>".format(e.sender_id)
         await e.reply(msg, parse_mode="html")
     else:
-        msg += "\n<a href='tg://user?id={}'>Done</a>".format(rmsg.sender_id)
+        msg += "\n📌 <a href='tg://user?id={}'>Done</a>".format(rmsg.sender_id)
         await rmsg.reply(msg, parse_mode="html")
 
     if len(files) < 2:
